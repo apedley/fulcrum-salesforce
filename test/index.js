e36bf0a632e892fae2b9e87fcb1f7b757ac0762f
apedley <apedley@gmail.com>
2016-03-11 16:59:17 -0600

    Added entry point

--- test/index.js
+++ test/index.js
@@ -3,7 +3,7 @@ var expect = require('chai').expect;
 
 describe('objects', function() {
   it('has student', function() {
-    expect(objects[0].name).to.equal('student');
-    expect(objects[1].name).to.equal('connection');
+    expect(objects.Student.name).to.equal('student');
+    expect(objects.Connection.name).to.equal('connection');
   });
 });
